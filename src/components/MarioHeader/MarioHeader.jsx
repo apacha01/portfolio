387ab1aa4683c9416dac9440dd5c71cff31327c0
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-09-02 11:46:05 -0300

    More responsive css. Adapt for large scale displays.

--- src/components/MarioHeader/MarioHeader.jsx
+++ src/components/MarioHeader/MarioHeader.jsx
@@ -1,6 +1,6 @@
 import './mario-header.css';
 import MISTERY_BLOCK_SVG from '../../assets/svgs/mario-mistery-block.svg';
-import PLAY_BUTTON_SVG from '../../assets/svgs/play.svg';
+import EXCLAMATION_BLOCK_SVG from '../../assets/svgs/mario-exclamation-block.svg';
 import COIN_GIF from '../../assets/imgs/coin.gif';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -15,27 +15,44 @@ function MarioHeader() {
 	useEffect(() => {
 		// dealy to let the coin out of the '?' block
 		setTimeout(() => document.getElementById('menu').classList.toggle('open'), 250);
+		setTimeout(() => {
+			let icon =  document.getElementById('menu-icon');
+			icon.src = open ? EXCLAMATION_BLOCK_SVG : MISTERY_BLOCK_SVG;
+		},250);
 	}, [open]);
 
 	return (
 		<header className="header-container">
 			<div className="header">
-				<img className="header-play-icon" src={PLAY_BUTTON_SVG} />
 				<p className="header-name">Agustin Pacheco</p>
 				<nav id="menu" className="menu-container">
 					<ul className="menu">
-						<li className="menu-link">Home</li>
-						<li className="menu-link">Experience</li>
-						<li className="menu-link">Skills</li>
-						<li className="menu-link">Studies</li>
-						<li className="menu-link">Contact</li>
+						<li className="menu-item">
+							<a className="menu-link" href="#" onClick={toggleOpen}>Home</a>
+						</li>
+						<li className="menu-item">
+							<a className="menu-link" href="#" onClick={toggleOpen}>Experience</a>
+						</li>
+						<li className="menu-item">
+							<a className="menu-link" href="#" onClick={toggleOpen}>Skills</a>
+						</li>
+						<li className="menu-item">
+							<a className="menu-link" href="#" onClick={toggleOpen}>Studies</a>
+						</li>
+						<li className="menu-item">
+							<a className="menu-link" href="#" onClick={toggleOpen}>Contact</a>
+						</li>
 					</ul>
 				</nav>
 				<button 
 					className={`header-menu-btn ${open ? 'animate' : ''}`} 
 					onClick={toggleOpen}
 				>
-					<img className="header-menu-icon" src={MISTERY_BLOCK_SVG} />
+					<img
+						id="menu-icon"
+						className="header-menu-icon"
+						src={MISTERY_BLOCK_SVG}
+					/>
 					<img className={`header-coin ${open ? 'animate' : ''}`} src={COIN_GIF} />
 				</button>
 				<div className="header-screw"></div>
