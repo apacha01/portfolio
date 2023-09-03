85b95c33e05f60f28c0ba7b76144981d4eeaf807
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-09-03 12:05:42 -0300

    Add animation to lang toggle. Add necesarry assets for the lang toggle and implemented it. Changes in css for responsivness.

--- src/components/MarioHeader/MarioHeader.jsx
+++ src/components/MarioHeader/MarioHeader.jsx
@@ -1,5 +1,7 @@
 import './mario-header.css';
 import MISTERY_BLOCK_SVG from '../../assets/svgs/mario-mistery-block.svg';
+import FLAG_US from '../../assets/imgs/flag-us.png';
+import FLAG_ARG from '../../assets/imgs/flag-arg.png';
 import COIN_GIF from '../../assets/imgs/coin.gif';
 import useLang from '../../contexts/langStore.js';
 import useOpenMenu from '../../hooks/useOpenMenu.js';
@@ -16,16 +18,17 @@ function MarioHeader() {
 		<div className="header-container">
 			<header className="header">
 				<p className="header-name">{text.headerName}</p>
-				<div className="lang-toggle-container">
-					<label htmlFor="lang" className="lang-toggle-label"></label>
-					<input
-						type="checkbox"
-						name="lang"
-						id="lang"
-						className="lang-toggle-input"
-						onClick={toggleLang}
+				<button 
+					className={`header-menu-btn ${open ? 'animate' : ''}`} 
+					onClick={toggleOpen}
+				>
+					<img
+						id="menu-icon"
+						className="header-menu-icon"
+						src={MISTERY_BLOCK_SVG}
 					/>
-				</div>
+					<img className={`header-coin ${open ? 'animate' : ''}`} src={COIN_GIF} />
+				</button>
 				<nav id="menu" className="menu-container">
 					<ul className="menu">
 						{text.headerLinks.map((e, i) => {
@@ -41,19 +44,21 @@ function MarioHeader() {
 								</li>
 							);
 						})}
+						<li className="lang-container">
+							<img src={FLAG_US} alt="United States flag" className="lang-flag" />
+							<div className="lang-toggle-container">
+								<input
+									type="checkbox"
+									id="lang"
+									className="lang-toggle-input"
+									onClick={toggleLang}
+								/>
+								<label htmlFor="lang" className="lang-toggle-label"></label>
+							</div>
+							<img src={FLAG_ARG} alt="Argentinian flag" className="lang-flag" />
+						</li>
 					</ul>
 				</nav>
-				<button 
-					className={`header-menu-btn ${open ? 'animate' : ''}`} 
-					onClick={toggleOpen}
-				>
-					<img
-						id="menu-icon"
-						className="header-menu-icon"
-						src={MISTERY_BLOCK_SVG}
-					/>
-					<img className={`header-coin ${open ? 'animate' : ''}`} src={COIN_GIF} />
-				</button>
 				<span className="header-screw"></span>
 				<span className="header-screw"></span>
 				<span className="header-screw"></span>
