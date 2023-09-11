a9898d11193dd7bbe1d14626b07b9188832936eb
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-09-11 18:28:43 -0300

    Add studies text in spanish. Refactor Studies component creating a Study component for each element.

--- src/components/Studies/Studies.jsx
+++ src/components/Studies/Studies.jsx
@@ -3,7 +3,7 @@ import Title from '../Title/Title';
 import useText from '../../hooks/useText';
 import MARIO_FLAGPOLE_TOP from '../../assets/svgs/mario-flagpole-top-sprite.svg';
 import MARIO_FLAGPOLE_BOTTOM from '../../assets/svgs/mario-flagpole-bottom-sprite.svg';
-import MARIO_FLAG from '../../assets/svgs/mario-flag-sprite.svg';
+import Study from '../Study/Study';
 
 function Studies() {
 	const text = useText();
@@ -14,24 +14,7 @@ function Studies() {
 			<div className="studies-container">
 				{
 					text.studies.map((e,i) => {
-						return (
-							<div key={i} className="study-container">
-								<div className="study-media-container">
-									<img
-										className='study-img'
-										src={e.img}
-										alt={`Image logo of the ${e.institution} institution`}
-									/>
-								</div>
-								<div className="study-text-container">
-									<h4 className="study-degree">* {e.degree}</h4>
-									<span className="study-time">* {e.timeSpend}</span>
-									<p className="study-institution">* {e.institution}</p>
-									<p className="study-location">* {e.location}</p>
-								</div>
-								<img src={MARIO_FLAG} alt="Mario flag" className="study-flag" />
-							</div>
-						);
+						return (<Study key={i} study={e} withFlag={true} />);
 					})
 				}
 				<div className="mario-flagpole">
---
+++ src/components/Study/Study.jsx
@@ -0,0 +1,29 @@
+import './study.css';
+import MARIO_FLAG from '../../assets/svgs/mario-flag-sprite.svg';
+
+function Study({ study, withFlag = false }) {
+	return (
+		<div className="study-container">
+			<div className="study-media-container">
+				<img
+					className='study-img'
+					src={study.img}
+					alt={`Image logo of the ${study.institution} institution`}
+				/>
+			</div>
+			<div className="study-text-container">
+				<h4 className="study-degree">* {study.degree}</h4>
+				<span className="study-time">* {study.timeSpend}</span>
+				<p className="study-institution">* {study.institution}</p>
+				<p className="study-location">* {study.location}</p>
+			</div>
+			{
+				withFlag
+					? <img src={MARIO_FLAG} alt="Mario flag" className="study-flag" />
+					: null
+			}
+		</div>
+	);
+}
+
+export default Study;
