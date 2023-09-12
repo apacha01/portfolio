fa07c87a8d789a5b22ec524570a91a0fcfdecb1e
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-09-12 18:14:40 -0300

    Add text for the about section. Add assets for later part of about section. Delete old asset.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,22 @@
 import useText from '../../hooks/useText';
 import Title from '../Title/Title';
 import './about.css';
+import MARIO_SIMPLE_BUSH from '../../assets/svgs/mario-single-bush-sprite.svg';
 
 function About() {
 	const text = useText();
 
 	return (
-		<section className="about-section">
+		<section id={text.headerLinks.about.id} className="about-section">
 			<Title title={text.headerLinks.about.title} withCloud={true} />
+			<div className="about-bush-container">
+				<img src={MARIO_SIMPLE_BUSH} alt="Mario Bush Sprite" className="bush" />
+				<img src={MARIO_SIMPLE_BUSH} alt="Mario Bush Sprite" className="bush" />
+			</div>
+			<div className="about-text-container">
+				<div className="text-overlay"></div>
+				<p className="about-text">{text.about.text}</p>
+			</div>
 		</section>
 	);
 }
