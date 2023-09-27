96eb357e6d5f32bff5639092b9c7aa6602d22365
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-09-27 04:21:00 -0300

    Changed fill to stroke on sprites to fix outline with color of the svg inner color.

--- src/components/Sprites/BushSprite.jsx
+++ src/components/Sprites/BushSprite.jsx
@@ -1,14 +1,9 @@
-import { THEME_OPTIONS } from '../../constants/theme.js';
-
-function BushSprite({ theme = 'light', classes = '' }) {
+function BushSprite({ classes = '' }) {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 960" className={classes}>
-			<g fill="#8cd600">
-				<path d="M840 0h240v60h60v60h60v120h60v-60h60v60h60v60h60v180h60v120h60v-60h60v-60h60v60h60v180h60v-60h60v60h60v180h-60v60H60v-60H0V780h60v-60h120V600h60v-60h60v-60h180v-60h60v-60h60V180h60v-60h120V60h60V0z" />
-				<path d="M780 120h30v30h-30v-30z" />
-			</g>
-			<path fill="#109400" d="M780 360h120v60H780v-60zM720 420h60v60h-60v-60zM1080 360h60v60h-60v-60zM1020 300h60v60h-60v-60z" />
-			<path fill={theme === THEME_OPTIONS.DARK ? '#084a00' : '#000000'} d="M840 0h240v60H840V0zm1020 720h60v180h-60V720zm-180-180h60v180h-60V540zM300 480h180v60H300v-60zm1080-180h60v180h-60V300zM600 180h60v180h-60V180zM0 780h60v120H0V780zm60-60h120v60H60v-60zm1380-240h60v120h-60V480zM1140 120h60v120h-60V120zm-480 0h120v60H660v-60zm1140 780h60v60h-60v-60zM60 900h60v60H60v-60zm1680-180h60v60h-60v-60zm60-60h60v60h-60v-60zm-300-60h60v60h-60v-60zm-1320 0h60v60h-60v-60zm1380-60h60v60h-60v-60zm-1320 0h60v60h-60v-60zm1380-60h60v60h-60v-60zM480 420h60v60h-60v-60zM540 360h60v60h-60v-60zM1320 240h60v60h-60v-60zm-120 0h60v60h-60v-60zm60-60h60v60h-60v-60zM1080 60h60v60h-60V60zm-300 0h60v60h-60V60z" />
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 16" className={classes}>
+			<path stroke='#000000' d="M14 .5h1M15 .5h1m1 0h1m0 1h1m-9 2h1m8 0h1m-10 1h1m9 0h1m-11 1h1m-2 1h1m13 0h1m-1 1h1m-18 1h1m20 0h1m-24 1h1m21 0h1m-2 1h1m2 0h1m-1 1h1m1 0h1m-30 1h1m29 0h1m-1 1h1m-1 1h1m-31 1h1M16 .5h1m-4 1h1m-3 1h2m8 1h1m0 1h1m-18 4h1m-3 2h1m25 2h1M19 2.5h1m-12 5h1m-2 1h1m-6 4h1m-3 2h1M23 5.5h1M24 8.5h1m3 1h1m-29 4h1m29 2h1M24 9.5h1" />
+			<path stroke="#8cd600" d="M14 1.5h1m-4 3h1m7 2h2m-2 1h1m-7 1h1m-7 1h1m-2 1h1m10 0h2m-10 1h1m5 0h1m8 0h1m-20 1h1m2 0h2m15 0h2m-21 1h1m7 0h1m2 0h1m3 0h1m2 0h1m-24 1h1m19 0h1m4 0h1m-2 1h1M15 1.5h3m-5 1h2m1 0h3m-8 1h5m1 0h1m-5 1h2m1 0h3m2 0h1m-10 1h4m2 0h1m1 0h3m-13 1h3m2 0h1m1 0h1m3 0h2m-14 1h2m2 0h1m1 0h4m1 0h1m1 0h1m-15 1h2m1 0h1m3 0h1m1 0h4m1 0h1m-18 1h2m1 0h4m1 0h6m2 0h3m3 0h1m-24 1h2m3 0h2m1 0h1m2 0h2m2 0h1m2 0h3m1 0h2m-25 1h6m1 0h5m3 0h2m1 0h2m2 0h3m-24 1h1m2 0h1m4 0h4m1 0h3m1 0h4m2 0h2m1 0h1m-30 1h1m1 0h2m3 0h1m2 0h3m1 0h2m1 0h3m1 0h2m1 0h5m-28 1h2m2 0h2m1 0h7m1 0h3m2 0h4m1 0h2m1 0h1m-29 1h3m2 0h6m1 0h4m3 0h2m1 0h1m1 0h1M15 2.5h1m5 6h1m-11 2h1m-3 3h1M16 3.5h1m1 0h1m-7 1h1m2 0h1m-5 1h1m7 0h1m-4 1h1m-6 1h1m2 0h1m-5 1h1m9 3h1m2 0h1m-13 1h1m4 0h1m3 0h1m-19 1h1m4 0h1m2 0h1m19 0h1m-15 1h1m3 0h1m-8 1h1m4 0h2m7 0h1M19 4.5h1m-4 1h1m-1 6h1m-14 1h1M21 7.5h1m-10 1h1m1 0h1m1 0h1m6 0h1m-12 1h1m6 0h2m-14 1h2m4 0h2m5 0h2m-5 1h1m-12 1h1m3 0h1m-6 1h1m-2 1h2m2 0h1m-4 1h2m13 0h1m2 0h1m4 0h2M29 14.5h1" />
+			<path stroke="#109400" d="M17 5.5h1M13 6.5h1M14 6.5h1M18 6.5h1M12 7.5h1" />
 		</svg>
 	);
 }
