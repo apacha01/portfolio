97260c7d97481542e459399d90e579ee5d3d6357
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2024-01-22 16:18:07 -0300

    Fix: theme reset on lang change. Store theme on localStorage.

---
+++ src/stores/theme.ts
@@ -0,0 +1,21 @@
+import { THEMES } from '../constants/theme.js';
+
+type Theme = typeof THEMES[keyof typeof THEMES];
+
+const defaultTheme = THEMES.OVERWORLD;
+
+export const getTheme = () => {
+	const t = localStorage.getItem('theme') || defaultTheme;
+
+	if (Object.values(THEMES).includes(t))
+		return t;
+	else
+		return defaultTheme;
+}
+
+export const setTheme = (theme: Theme) => {
+	if (Object.values(THEMES).includes(theme))
+		localStorage.setItem('theme', theme);
+	else
+		localStorage.setItem('theme', defaultTheme);
+}
