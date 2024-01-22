f805ab615db3b884af491f6efb6072167db8ec38
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2024-01-22 16:33:32 -0300

    Check user prefered color scheme.

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -2,7 +2,7 @@ import { THEMES } from '../constants/theme.js';
 
 type Theme = typeof THEMES[keyof typeof THEMES];
 
-const defaultTheme = THEMES.OVERWORLD;
+const defaultTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? THEMES.UNDERGROUND : THEMES.OVERWORLD;
 
 export const getTheme = () => {
 	const t = localStorage.getItem('theme') || defaultTheme;
